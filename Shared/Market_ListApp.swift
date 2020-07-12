a8536065e7ca6ec2dfa21e815ec862f4b753d3ba
Arsalan <airavani2018@ada.edu.az>
2020-07-12 19:52:17 +0200

    Initial Commit

---
+++ Shared/Market_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Market_ListApp.swift
+//  Shared
+//
+//  Created by Arsalan Iravani on 12.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct Market_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
